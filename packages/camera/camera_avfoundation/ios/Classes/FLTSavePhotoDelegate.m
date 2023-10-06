122f02cffae9beb9dda4b9c69c9a0354dac98af1
Shunta Iketaki <s.iketaki@gmail.com>
2023-10-07 02:49:21 +0900

    feat: make the shutter sound of iOS silent

--- packages/camera/camera_avfoundation/ios/Classes/FLTSavePhotoDelegate.m
+++ packages/camera/camera_avfoundation/ios/Classes/FLTSavePhotoDelegate.m
@@ -55,4 +55,9 @@
                         }];
 }
 
+- (void)captureOutput:(AVCapturePhotoOutput *)output
+    willCapturePhotoForResolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings {
+  AudioServicesDisposeSystemSoundID(1108);
+}
+
 @end
